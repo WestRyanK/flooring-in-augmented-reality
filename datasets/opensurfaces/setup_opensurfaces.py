c183992b5fc5b794dd31dbd8f78aa911f98f43d0
WestRyanK <west.ryan.k@gmail.com>
2021-11-15 14:54:27 -0700

    Initial setup for OpenSurfaces Dataset

---
+++ datasets/opensurfaces/setup_opensurfaces.py
@@ -0,0 +1,29 @@
+import urllib2
+import os
+import zipfile
+import process_opensurfaces
+
+def download_extract_opensurfaces_dataset():
+    datasets_dir = os.path.join('datasets', 'opensurfaces')
+    opensurfaces_zip_path = os.path.join(datasets_dir, 'opensurfaces.zip')
+    opensurfaces_url = 'http://labelmaterial.s3.amazonaws.com/release/opensurfaces-release-0.zip'
+    opensurfaces_root_dir = os.path.join(datasets_dir, 'opensurfaces')
+    if not os.path.exists(opensurfaces_zip_path):
+        with open(opensurfaces_zip_path, 'wb') as zip_file:
+            print "Downloading OpenSurfaces Dataset..."
+            zip_file.write(urllib2.urlopen(opensurfaces_url).read())
+            print "Download Complete"
+    else:
+        print "OpenSurfaces already downloaded"
+    if not os.path.exists(opensurfaces_root_dir):
+        os.makedirs(opensurfaces_root_dir)
+        with zipfile.ZipFile(opensurfaces_zip_path, 'r') as zip_file:
+            print "Unzipping OpenSurfaces Dataset..."
+            zip_file.extractall(datasets_dir)
+            print "Unzip Complete"
+    else:
+        print "OpenSurfaces already unzipped"
+
+
+download_extract_opensurfaces_dataset()
+process_opensurfaces.process_opensurfaces()
