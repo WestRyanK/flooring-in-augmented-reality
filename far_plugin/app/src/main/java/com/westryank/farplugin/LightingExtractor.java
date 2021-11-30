36c260aacba74bd3eb0056d2118857da2b2235ea
WestRyanK <west.ryan.k@gmail.com>
2021-11-29 17:24:19 -0700

    Fix LightingExtractor

--- far_plugin/app/src/main/java/com/westryank/farplugin/LightingExtractor.java
+++ far_plugin/app/src/main/java/com/westryank/farplugin/LightingExtractor.java
@@ -5,16 +5,21 @@ import android.util.Log;
 
 import org.opencv.android.Utils;
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LightingExtractor {
     private static final String TAG = "Far:LightingExtractor";
 
-    private void Clamp(Mat inMat)
-    {
+    private void Clamp(Mat inMat) {
         Imgproc.threshold(inMat, inMat, 0, 255, Imgproc.THRESH_TOZERO);
         Imgproc.threshold(inMat, inMat, 255, 255, Imgproc.THRESH_TRUNC);
     }
@@ -25,27 +30,67 @@ public class LightingExtractor {
         Mat maskMat = new Mat();
         Utils.bitmapToMat(inMask, maskMat);
         Core.extractChannel(maskMat, maskMat, 0);
+        Core.rotate(maskMat, maskMat, Core.ROTATE_180);
+        Core.absdiff(maskMat, new Scalar(255), maskMat);
+        Mat blurredMaskMat = new Mat();
+        maskMat.convertTo(blurredMaskMat, CvType.CV_32FC1, 1.0f / 255);
 
+        Size blurKernel = new Size(11, 11);
         Imgproc.cvtColor(imageMat, imageMat, Imgproc.COLOR_BGRA2GRAY);
-        Imgproc.blur(imageMat, imageMat, new Size(11, 11));
-        Log.v(TAG, OpenCVUtils.GetMatDetails("ImageMat", imageMat));
-//        Scalar avgBrightness = Core.mean(imageMat, maskMat);
-//        Scalar avgBrightnessOffset = new Scalar(128 - avgBrightness.val[0]);
-//        Log.v(TAG, OpenCVUtils.GetMatDetails("Pre Offset ImageMat", imageMat));
-//        Core.add(imageMat, avgBrightnessOffset, imageMat);
-//        Log.v(TAG, OpenCVUtils.GetMatDetails("Post Offset ImageMat", imageMat));
-//        Clamp(imageMat);
-//        Log.v(TAG, OpenCVUtils.GetMatDetails("Post Clamp ImageMat", imageMat));
+        Core.multiply(imageMat, maskMat, imageMat, 1.0f / 255);
 
-        Imgproc.cvtColor(imageMat, imageMat, Imgproc.COLOR_GRAY2BGRA);
+        Imgproc.blur(imageMat, imageMat, blurKernel);
+        Imgproc.blur(blurredMaskMat, blurredMaskMat, blurKernel);
+
+        imageMat.convertTo(imageMat, CvType.CV_32FC1);
+        Core.divide(imageMat, blurredMaskMat, imageMat);
+        imageMat.convertTo(imageMat, CvType.CV_8UC1);
+
+        Scalar avgBrightness = Core.mean(imageMat, maskMat);
+//        Scalar medBrightness = new Scalar(median(imageMat, maskMat));
+        Scalar avgBrightnessOffset = new Scalar(128 - avgBrightness.val[0]);
+        Core.add(imageMat, avgBrightnessOffset, imageMat);
+        Clamp(imageMat);
+
         Core.rotate(imageMat, imageMat, Core.ROTATE_180);
+        Imgproc.cvtColor(imageMat, imageMat, Imgproc.COLOR_GRAY2BGRA);
 
         Bitmap outputBitmap = Bitmap.createBitmap(inImage.getWidth(), inImage.getHeight(), Bitmap.Config.ARGB_8888);
         Utils.matToBitmap(imageMat, outputBitmap);
 
         imageMat.release();
         maskMat.release();
+        blurredMaskMat.release();
 
         return outputBitmap;
+    }
+
+    double median(Mat inMat, Mat inMask) {
+        int binCount = 256;
+        int maxValue = 255;
+        List<Mat> imgs = new ArrayList<>();
+        imgs.add(inMat);
+        MatOfInt channels = new MatOfInt(0);
+        Mat histogram = new Mat();
+        MatOfInt histogramSize = new MatOfInt(binCount);
+        MatOfFloat histogramRanges = new MatOfFloat(0, maxValue);
+        Imgproc.calcHist(imgs, channels, inMask, histogram, histogramSize, histogramRanges, false);
+
+        float[] valueArray = new float[1];
+        double histogramTotal = Core.sumElems(histogram).val[0];
+        long halfCount = (long)(histogramTotal / 2);
+        long sumTotal = 0;
+        int index = 0;
+        for (int i = 0; i < binCount; i++) {
+            histogram.get(i, 0, valueArray);
+            sumTotal += valueArray[0];
+            if (sumTotal > halfCount) {
+                index = i;
+                break;
+            }
+        }
+        float binPercent = ((float)(index) / binCount);
+        int medianValue = (int)(binPercent * maxValue);
+        return medianValue;
     }
 }
